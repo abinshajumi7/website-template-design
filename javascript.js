9137523f65e15ece8f613d280ea61c2253c705c5
abinshajumi7 <abinshajumi7@gmail.com>
2026-02-17 11:37:06 +0530

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,20 @@
+const abin=[1,2,3,"hy","hello"]
+abin.push("audi");
+console.log(abin);
+
+let x=6
+let y=4
+z=x+y
+console.log(z);
+
+
+const car={
+    type: "fiat",
+    model:799,
+    color:"white"
+};
+
+console.log(car);
+
+
+
